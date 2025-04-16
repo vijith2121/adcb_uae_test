f2d626c8849c991146e5a6246d87e810e7a7530e
vijith2121 <vijisaj2121@gmail.com>
2025-04-16 14:17:38 +0530

    update input Items

--- adcb_uae_test/spiders/main.py
+++ adcb_uae_test/spiders/main.py
@@ -426,7 +426,7 @@ class Adcb_uae_testSpider(scrapy.Spider):
             Bucket_greater_6_amount = Bucket_greater_6_amount.split('</td>')[0].strip()
 
         # scrape_date = date.today()
-        scrape_date = '2025-04-13'
+        scrape_date = '2025-04-14'
         items = parser.xpath(xpath_data)
         data = {}
         for item in items:
